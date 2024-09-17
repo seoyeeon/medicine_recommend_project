d3d0ca03409f5139cc984ae7613f05ac275412c6
seoyeeon <shift2013@naver.com>
2024-09-17 15:04:34 +0900

    서버 문제 해결

--- medicine_recommend_project/server.js
+++ medicine_recommend_project/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 5000;
+const port = 3000;
+
+// JSON 요청 처리 미들웨어
+app.use(express.json());
 
 // MySQL 연결 설정
 const db = mysql.createConnection({
@@ -20,7 +23,7 @@ db.connect(err => {
 });
 
 // API 엔드포인트 설정 (약 목록 가져오기)
-app.get('/api/medicines', (req, res) => {
+app.get('/medicines', (req, res) => {
   const sql = 'SELECT * FROM OTC_Medicines';
   db.query(sql, (err, results) => {
     if (err) {
@@ -29,6 +32,23 @@ app.get('/api/medicines', (req, res) => {
     res.json(results);
   });
 });
+
+app.get('/medicines', (req, res) => {
+  console.log('GET /medicines 요청이 들어왔습니다.');
+  const sql = 'SELECT * FROM OTC_Medicines';
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).send('데이터베이스 쿼리 오류');
+    }
+    console.log('데이터베이스 쿼리 성공:', results);
+    res.json(results);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('서버가 정상적으로 동작 중입니다!');
+});
+
 
 app.listen(port, () => {
   console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
